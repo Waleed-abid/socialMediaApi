08f03442c590c81743edcb1e0a7bf03186420786
Waleed-abid <waleedmain9@gmail.com>
2022-11-09 17:42:11 +0500

    updated env vars

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config;
 const connectDB = async () => {
   const conn = await mongoose.connect(
-    "mongodb+srv://Waleedabid:Waleed123@cluster0.kzkfk.mongodb.net/social?retryWrites=true&w=majority"
+    process.env.mongoUri
   );
 
   console.log(`Mongo DB Connected: ${conn.connection.host}`);
